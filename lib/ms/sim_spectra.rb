705d92d77cbdb05e1adde95cb198060f3e13fc5e
A Noyce <andrewbnoyce@gmail.com>
2012-07-03 21:30:09 -0600

    Merging overlapping peaks

--- lib/ms/sim_spectra.rb
+++ lib/ms/sim_spectra.rb
@@ -1,8 +1,4 @@
 
-require 'ms/rt/rtgenerator'
-require 'ms/sim_feature'
-require 'ms/noise'
-
 module MS
   class Sim_Spectra
     def initialize(peptides,sampling_rate, run_time, drop_percentage = 0.12,density = 10.0,one_d = false)
@@ -44,11 +40,74 @@ module MS
 	  @spectra[k] = [n_v[0],n_v[1]]
 	end
       end
+      
+      #-----------------------------------------------------------------
+      #Detect overlapping peaks:
+      #Weighted avg for m/z - Sum Intensities
+      @start = Time.now
+      new_spectra = {}
+      count = 0
+      @spectra.each do |k,arr|
+	Progress.progress("Merging Overlaps:",((count/@spectra.size.to_f)*100).to_i)
+	overlaps = []
+	mzs = arr[0].clone
+	ints = arr[1].clone
+	max = mzs.max
+	min = mzs.min
+	mzs.clone.each do |mz|
+	  group = mzs.group_by{|m| ((mz-0.01)..(mz+0.01)).member?(m)}[true]
+	  if group.size > 1
+	    group_i = group.map{|m| mzs.index(m)}
+	    overlaps<<group_i
+	  end
+	end
+	overlaps.uniq!
+	#bigger groups than two
+	overlaps.clone.combination(2) do |a,b|
+	  c = a.size + b.size
+	  if c != (a+b).uniq.size
+	    if a.size > b.size
+	      overlaps.delete(b)
+	    else
+	      overlaps.delete(a)
+	    end
+	  end
+	end
+	#end
+	if !overlaps.empty?
+	  overlaps.each do |ol|
+	    new_int = 0
+	    new_mz = 0
+	    n_mzs = []
+	    n_ints = []
+	    ol.each{|i| new_int += ints[i]; n_mzs<<mzs[i]; n_ints<<ints[i]}
+	    new_mz = w_avg(n_mzs,n_ints)
+	    ol.each{|i| ints[i] = nil; mzs[i] = nil}
+	    ints<<new_int
+	    mzs<<new_mz
+	  end
+	end
+	new_spectra[k] = [mzs.compact,ints.compact]
+	count += 1
+      end
+      @spectra = new_spectra
+      Progress.progress("Merging Overlaps:",100,Time.now-@start)
+      puts ""
+      #-----------------------------------------------------------------
+      
       return @noise
     end
     
     def self.r_times
       @@r_times
+    end
+    
+    def w_avg(values,weights)
+      a = []
+      values.each_with_index{|v,i| a<<v*weights[i]}
+      a = a.inject(:+)
+      b = weights.inject(:+)
+      return a/b
     end
     
     attr_reader :data, :max_mz, :spectra, :noise, :features
