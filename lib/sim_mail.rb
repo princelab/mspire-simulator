336ec50c021be60e25502977e7cf4d4fbb855f7e
A Noyce <andrewbnoyce@gmail.com>
2012-08-10 16:10:39 -0600

    Rearange

---
+++ lib/sim_mail.rb
@@ -0,0 +1,26 @@
+
+require 'pony'
+
+begin
+  address = ARGV[0]
+  msgcount = ARGV.count - 1
+  msgbody = ""
+
+  for i in 1..msgcount
+    msgbody << " #{ARGV[i]}"
+  end
+
+  Pony.mail(:to => address, :via => :smtp, :via_options => {
+    :address              => 'smtp.gmail.com',
+    :port                 => '587',
+    :enable_starttls_auto => true,
+    :user_name            => 'mspire.simulator',
+    :password             => 'chromatography',
+    :authentication       => :plain, 
+    :domain               => "localhost.localdomain" 
+    }, 
+    :subject => 'Mspire-Simulator', :body => msgbody
+  )
+rescue
+  puts "Email function failed. Check email address and internet connection."
+end
