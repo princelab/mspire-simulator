b5765bb5b0b43e63929f79d11be4627a9a67e101
Ryan Taylor <ryanmt@byu.net>
2012-10-24 17:25:16 -0600

    Made the Progress module more extensible

--- lib/progress.rb
+++ lib/progress.rb
@@ -1,25 +1,28 @@
 
 module Progress
   module_function
-  # TODO Ryan: update this to handle a single message instantiation and then updates in the progress... Then it won't have to rewrite the message as often, nor process things as often
-  def progress(message, num, time = '')
-    # move cursor to beginning of line
-    cr = "\r"           
-
-    # ANSI escape code to clear line from cursor to end of line
-    # "\e" is an alternative to "\033"
-    # cf. http://en.wikipedia.org/wiki/ANSI_escape_code
-    clear = "\e[0K"     
-
-    # reset lines
-    reset = cr + clear
-    if time == ''
-      print "#{reset} #{message}" + "#{num}%".rjust(60-message.length)
-      $stdout.flush
-    else
-      str = "#{reset} #{message}" + "#{num}%".rjust(60-message.length)
-      print  str + "Took: #{"%.2f" % time} sec.".rjust(100-str.length)
-      $stdout.flush
-    end
+  # ANSI escape code to clear line from cursor to end of line
+  # "\e" is an alternative to "\033"
+  # cf. http://en.wikipedia.org/wiki/ANSI_escape_code
+  @reset = "\r\e[OK"
+  def initialize(message, time = nil)
+    @message = message
+    @time = time ? time : Time.now
   end
+  def progress(num, message_addition = nil)
+    # move cursor to beginning of line
+    message = @message+message_addition
+    # reset lines
+    print "#{@reset} #{message}" + "#{num}%".rjust(60-message.length)
+    $stdout.flush
+  end
+  def finish!(message_addition = nil)
+    time = Time.now - @time
+    message = @message + message_addition
+    str = "#{@reset} #{message}" + "#{num}%".rjust(60-message.length)
+    print  str + "Took: #{"%.2f" % time/1000.0} sec.".rjust(100-str.length)
+    $stdout.flush
+    @message, @time = nil, nil
+  end
+  alias :progress, :update
 end
